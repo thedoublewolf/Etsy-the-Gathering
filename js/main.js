a6635565708898401defc70100dabdac25df313f
Jonny Warren <jonnywarren06@gmail.com>
2015-10-07 16:43:01 -0400

    header and side nav up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 (function () {
 
-  console.log('It Works!');
+  var firstItem = items.results[0];
+
+  var templateString = $('itemTemplate').text();
+
+  var templateFunction = _.template(templateString);
+
+  _.each(items.results, function (item){
+  	 var itemHTML = templateFunction(item);
+  	$('.magicStuff').append(itemHTML);
+	});
 
 }());

d8a0441c4fc80c81f7d101ab1f101f21595c55f4
Jonny Warren <jonnywarren06@gmail.com>
2015-10-10 13:49:30 -0400

    Mouseover and Out effects completed.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,27 @@
 (function () {
-
+	// Grab the template string
   var templateString = $('#itemListing').text();
-
+  // Turn it into a template function
   var renderTemplate = _.template(templateString);
-
+  // For each item, return value as strings
+  // Append strings to #itemListing Script tag 
   _.each(items.results, function (item){
   	 var itemHTML = renderTemplate(item);
   	$('.magicStuff').append(itemHTML);
 	});
 
-	$('.magicItem').each( function (){
-		$('.magicItem').mouseenter( function (){
-			$('#heart').removeClass('heartHidden').addClass('heartShown');
-			$('#hamburger').removeClass('hamburgerHidden').addClass('hamburgerShown');
-		});
-	});
 
-	// $('.magicItem').each( function (){
-	// 	$('.magicItem').mouseover( function (){
-	// 		$('#heart').removeClass('heartShown').addClass('heartHidden');
-	// 		$('#hamburger').removeClass('hamburgerShown').addClass('hamburgerHidden');
-	// 	});
-	// });
+  // On each item on mouseover, find the descendant and remove/add class
+  $('.magicItem').mouseover( '.heartHam', function (event){
+  	console.log(event);
+  	$(this).find('#heart').removeClass('heartHidden').addClass('heartShown');
+  	$(this).find('#hamburger').removeClass('hamburgerHidden').addClass('hamburgerShown');
+  });
+  // On each item on mouseout, find the descendant and add/remove class
+  $('.magicItem').mouseout( '.heartHam', function (event){
+  	console.log(event);
+  	$(this).find('#heart').addClass('heartHidden').removeClass('heartShown');
+  	$(this).find('#hamburger').addClass('hamburgerHidden').removeClass('hamburgerShown');
+  });
 
 }());
